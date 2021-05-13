6dd36e880234eadc2687b7dbfd8f3dcd2f8471c2
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2021-05-13 15:06:41 +0530

    Create NamedNanonymousCall_21.sol

---
+++ NamedNanonymousCall_21.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.0 <0.9.0;
+
+contract C {
+    mapping(uint => uint) data;
+
+    function f() public {
+        set({value: 2, key: 3});
+    }
+
+    function set(uint key, uint value) public {
+        data[key] = value;
+    }
+    
+    // Omited parameter example
+    function checkMap(uint key, uint) view public returns(uint){
+        return data[key];
+    }
+}
